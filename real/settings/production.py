aa5567663ff78f8495f95c47c166e6e7e7af2844
breezil <kolajubril@gmail.com>
2018-10-31 11:11:43 -0400

    still debugging

--- real/settings/production.py
+++ real/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 

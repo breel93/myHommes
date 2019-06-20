3577c189c4781394f01b72f3d5ae524bc0866933
kola <kolajubril@gmail.com>
2019-06-21 00:32:49 +0100

    Fix: fix for app crashes

--- real/settings/production.py
+++ real/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.myhommes.com' , '.herokuapp.com']
 

3289b6e923eb8fc57dfd61901b9e8ac65cb8336d
kola <kolajubril@gmail.com>
2019-06-21 00:31:59 +0100

    Fix: fix for app crashes

--- real/settings/production.py
+++ real/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.myhommes.com']
+ALLOWED_HOSTS = ['.myhommes.com' , '.herokuapp.com']
 
 
 # Application definition
